4bbfab04abef0c7d533930969a6ab9145356a2b3
Michael Mainer <mmainer@microsoft.com>
2017-12-18 12:37:06 -0800

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # msgraph-sdk-dotnet-beta
 Class library solution for creating beta models that can be consumed by the production V1.0 client library. This repo will also contain a script that people can use to help generate beta models.
+
+## How to create beta models for consumption with the .Net client library
---
+++ msgraph-sdk-dotnet-beta-models/Custom/CustomClass.cs
@@ -0,0 +1,22 @@
+﻿// This file is provided in case you need to add custom models that are not described by the 
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+// Your beta models must be in this namepspace.
+namespace Microsoft.Graph
+{
+    // Use the JsonObjectAttribute for all models you want serialized. Check whether it inherits from other models.
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public partial class CustomClass
+    {
+        // Use this as a template for adding a property that returns a collection.
+        //[JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "collectionOfObjects", Required = Newtonsoft.Json.Required.Default)]
+        //public IEnumerable<MyObjectType> CollectionOfObjects { get; set; }
+
+        //[JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "myProperty", Required = Newtonsoft.Json.Required.Default)]
+        //public string MyProperty { get; set; }
+
+    }
+}
